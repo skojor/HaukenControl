32d733e1beeecd42eeafe2888f571b52788e2c2a
Jørn Skorstad <jsk@nkom.no>
2025-04-30 08:29:29 +0200

    Removed build folder

--- build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_HaukenControl_FILE C:/Kode/kode/kode/kode/kode/HaukenControl/build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/HaukenControl.exe)
-set(__QT_DEPLOY_TARGET_HaukenControl_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_HaukenControl_RUNTIME_DLLS C:/Qt/6.9.0/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.0/msvc2022_64/bin/Qt6Cored.dll)
